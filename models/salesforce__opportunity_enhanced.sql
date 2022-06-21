1b026344ce3d739fc6947a85bebb54fc79a2f6e1
fivetran-reneeli <renee.li@fivetran.com>
2022-06-21 14:55:04 -0400

    add user role to opp enhanced model

--- models/salesforce__opportunity_enhanced.sql
+++ models/salesforce__opportunity_enhanced.sql
@@ -10,6 +10,15 @@ salesforce_user as (
     from {{ var('user') }}  
 ), 
 
+-- If using user_role table, the following will be included, otherwise it will not.
+{% if var('salesforce__user_role_enabled', True) %}
+user_role as (
+
+    select *
+    from {{ var('user_role') }}  
+), 
+{% endif %}
+
 account as (
 
     select *
@@ -34,6 +43,15 @@ add_fields as (
         opportunity_manager.user_name as opportunity_manager_name,
         opportunity_manager.city opportunity_manager_city,
         opportunity_manager.state as opportunity_manager_state,
+
+        -- If using user_role table, the following will be included, otherwise it will not.
+        {% if var('salesforce__user_role_enabled', True) %}
+        user_role.user_role_name as position, 
+        user_role.developer_name,
+        user_role.parent_role_id,
+        user_role.rollup_description,
+        {% endif %}
+
         case
             when opportunity.is_won then 'Won'
             when not opportunity.is_won and opportunity.is_closed then 'Lost'
@@ -63,6 +81,8 @@ add_fields as (
         on opportunity.owner_id = opportunity_owner.user_id
     left join salesforce_user as opportunity_manager 
         on opportunity_owner.manager_id = opportunity_manager.user_id
+    left join user_role 
+        on opportunity_owner.user_id = user_role.user_role_id
 )
 
 select *
