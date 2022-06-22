5b5c3495130b99d5acb23cb467d72d828da9b669
fivetran-reneeli <renee.li@fivetran.com>
2022-06-21 23:12:02 -0400

    add account data

--- models/salesforce__contact_enhanced.sql
+++ models/salesforce__contact_enhanced.sql
@@ -4,6 +4,12 @@ with contact as (
     from {{ var('contact') }}
 ), 
 
+account as (
+
+    select *
+    from {{ var('account') }}
+)
+
 select 
     _fivetran_synced,
     contact_id,
@@ -19,6 +25,30 @@ select
     name,
     owner_id,
     phone,
-    reports_to_id
+    reports_to_id,
+    account.account_number,
+    account.account_source,
+    account.annual_revenue,
+    account.billing_city,
+    account.billing_country,
+    account.billing_postal_code,
+    account.billing_state,
+    account.billing_state_code,
+    account.billing_street,
+    account.description,
+    account.industry,
+    account.is_deleted,
+    account.last_activity_date,
+    account.account_name,
+    account.number_of_employees,
+    account.owner_id,
+    account.ownership,
+    account.parent_id,
+    account.rating,
+    account.record_type_id,
+    account.type,
+    account.website
 
 from contact
+left join account 
+    on contact.account_id = account.id
