055cc41db6867d4d5a276f60fa304db781ce538f
fivetran-reneeli <renee.li@fivetran.com>
2022-06-27 14:03:39 -0400

    update daily funnel

--- models/salesforce__daily_funnel.sql
+++ models/salesforce__daily_funnel.sql
@@ -6,7 +6,7 @@ with task as (
     select 
         task_id,
         account_id,
-        activity_date,
+        {{ dbt_utils.date_trunc('day', 'activity_date') }} as activity_date,
         completed_date_time,
         is_closed,
         is_deleted,
@@ -22,7 +22,7 @@ salesforce_event as (
     select 
         event_id,
         account_id,
-        activity_date,
+        {{ dbt_utils.date_trunc('day', 'activity_date') }} as activity_date,
         created_date,
         event_subtype,
         owner_id,
@@ -36,7 +36,7 @@ salesforce_lead as (
         lead_id,
         converted_account_id,
         owner_id,
-        created_date,
+        {{ dbt_utils.date_trunc('day', 'created_date') }} as created_date,
         status
 
     from {{ var('lead') }}
@@ -48,27 +48,28 @@ salesforce_converted_lead as (
         lead_id,
         converted_account_id,
         owner_id,
-        converted_date,
+        {{ dbt_utils.date_trunc('day', 'converted_date') }} as converted_date,
         status
     from {{ var('lead') }}
+    where is_converted is true
+
 ), 
 
 account as (
     select
         account_id,
-        last_activity_date,
         owner_id,
         type
     from {{ var('account') }}
 ),
 
-opportunity as (
+opportunity_open as (
 
     select 
         opportunity_id,
-        created_date,
-        close_date,
+        {{ dbt_utils.date_trunc('day', 'created_date') }} as created_date,
         account_id,
+        close_date,
         is_closed,
         is_deleted,
         is_won,
@@ -76,15 +77,17 @@ opportunity as (
         stage_name,
         type
     from {{ var('opportunity') }}
-    where is_won is true
 )
 
+
+
 select
-    salesforce_lead.created_date,	
-    salesforce_lead.lead_id,
+    -- salesforce_lead.created_date,	
+    -- salesforce_lead.lead_id
+    -- ,
     
-    opportunity.opportunity_id,
-    opportunity.stage_name
+    -- opportunity.opportunity_id,
+    -- opportunity.stage_name
     -- ,
     -- count(distinct salesforce_converted_lead.converted_account_id) as converted_account_count,
     -- count(task.task_id) as task_count,
@@ -92,13 +95,25 @@ select
     -- count(distinct(case when opportunity.is_won then opportunity.opportunity_id else null end)) as won_opportunity_count 
 
 
-    from salesforce_lead
+    -- from salesforce_lead
     -- left join salesforce_converted_lead
     --     on salesforce_lead.created_date = salesforce_converted_lead.converted_date
     -- left join task 
     --     on salesforce_lead.created_date = task.activity_date
     -- left join salesforce_event
     --     on salesforce_lead.created_date = salesforce_event.activity_date
-    left join opportunity
-        on salesforce_lead.converted_account_id = opportunity.account_id
+    -- left join opportunity
+    --     on salesforce_lead.converted_account_id = opportunity.account_id
         -- on salesforce_lead.created_date = opportunity.close_date -- concern is if this doesn't exactly equal the date the opportunity was won, need to figure that out
+
+
+    salesforce_lead.lead_id,
+    salesforce_converted_lead.converted_account_id,
+    opportunity.opportunity_id 
+
+    from salesforce_lead, salesforce_converted_lead,opportunity
+
+    where 
+    salesforce_lead.created_date = cast(current_date as {{ dbt_utils.type_timestamp() }}) 
+    and salesforce_converted_lead.converted_date = cast(current_date as {{ dbt_utils.type_timestamp() }}) 
+    and opportunity.created_date = cast(current_date as {{ dbt_utils.type_timestamp() }}) 
